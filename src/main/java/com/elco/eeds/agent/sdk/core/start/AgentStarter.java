33c71fcf92977b2a4f7fd068e6e317a7c6e42748
yangtianlei <547070284@qq.com>
2022-12-21 10:07:10 +0800

    协议注入

--- src/main/java/com/elco/eeds/agent/sdk/core/start/AgentStarter.java
+++ src/main/java/com/elco/eeds/agent/sdk/core/start/AgentStarter.java
@@ -5,6 +5,7 @@ import com.elco.eeds.agent.sdk.core.bean.agent.Agent;
 import com.elco.eeds.agent.sdk.core.bean.agent.AgentBaseInfo;
 import com.elco.eeds.agent.sdk.core.common.constant.ConstantFilePath;
 import com.elco.eeds.agent.sdk.core.common.enums.ErrorEnum;
+import com.elco.eeds.agent.sdk.core.connect.init.InitConnectFactory;
 import com.elco.eeds.agent.sdk.core.exception.SdkException;
 import com.elco.eeds.agent.sdk.core.util.read.parameterfile.AgentConfigYamlReader;
 import com.elco.eeds.agent.sdk.core.util.read.parameterfile.ResourceLoader;
@@ -33,12 +34,16 @@ public class AgentStarter {
 
     private static ThingsSyncIncrMessageHandler thingsSyncIncrMessageHandler = new ThingsSyncIncrMessageHandler(thingsSyncService);
 
-    private static AgentRegisterService registerService = new AgentRegisterService(thingsSyncService,thingsSyncIncrMessageHandler);
+    private static AgentRegisterService registerService = new AgentRegisterService(thingsSyncService, thingsSyncIncrMessageHandler);
 
     private AgentConfigYamlReader configReader;
 
     private static void init(AgentStartProperties agentStartProperties) throws Exception {
         try {
+            // 将协议注入
+            InitConnectFactory.addConnectPackagePath(agentStartProperties.getProtocolPackage());
+            InitConnectFactory.initConnect();
+
             Agent.getInstance().setAgentBaseInfo(new AgentBaseInfo(agentStartProperties));
             // 注册
             registerService.register(agentStartProperties.getServerUrl(), agentStartProperties.getName(),
@@ -61,11 +66,12 @@ public class AgentStarter {
 
     /**
      * 客户端手动启动方法
-     * @param serverUrl     server地址
-     * @param name          客户端名称
-     * @param port          客户端端口
-     * @param token         客户端token
-     * @param baseFolder    存储文件目录
+     *
+     * @param serverUrl  server地址
+     * @param name       客户端名称
+     * @param port       客户端端口
+     * @param token      客户端token
+     * @param baseFolder 存储文件目录
      * @throws Exception
      */
     public static void init(String serverUrl
@@ -74,7 +80,7 @@ public class AgentStarter {
             , String token
             , String baseFolder
             , String clientType
-            ,String protocolPackage
+            , String protocolPackage
     ) throws Exception {
         logger.debug("开始手动初始化方法...");
         logger.debug("传入参数为：服务器地址：{}，客户端名称：{}，客户端端口：{}，token：{}，文件存储路径：{}",
@@ -94,6 +100,7 @@ public class AgentStarter {
 
     /**
      * 客户端手动启动方法：一个参数（从给定的路径取yml文件）
+     *
      * @param ymlPath yml的绝对路径
      */
     public static void init(String ymlPath) throws Exception {
@@ -119,12 +126,12 @@ public class AgentStarter {
         AgentStartProperties agentStartProperties = AgentStartProperties.getInstance();
 
         String fileName = getJarSamePathYml();
-        if(FileUtil.exist(new File(fileName))) {
+        if (FileUtil.exist(new File(fileName))) {
             // 默认在jar包相同路径下读取agent-sdk-config.yaml
             agentStartProperties = agentConfigYamlReader.parseYaml(fileName);
             logger.debug("jar包同级路径配置文件读取成功");
             logger.info("读取配置文件成功：{}", agentStartProperties.toString());
-        }else {
+        } else {
             // jar包中resource文件下的agent-sdk-config.yaml
             logger.debug("jar包同级路径配置文件不存在，即将开始读取jar包中resource文件下的配置文件");
             agentStartProperties = agentConfigYamlReader.parseYaml("./" + ConstantFilePath.YML_NAME);
@@ -141,6 +148,7 @@ public class AgentStarter {
 
     /**
      * 获取jar包同路径下的规定名称的配置文件
+     *
      * @return
      */
     private static String getJarSamePathYml() {
@@ -149,7 +157,7 @@ public class AgentStarter {
         //默认是linux os
         String fileName = "/" + ConstantFilePath.YML_NAME;
         //判断是否是windows os
-        if(System.getProperty ("os.name").contains("Windows")) {
+        if (System.getProperty("os.name").contains("Windows")) {
             fileName = "\\" + ConstantFilePath.YML_NAME;
         }
         // 读取当前目录下conf配置文件
